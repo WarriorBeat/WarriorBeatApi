90bc2bce2a12566cffe0daf8136dbaeebcd20deb
Braden Mars <bradenmars02@gmail.com>
2018-12-03 19:08:43 -0600

    Package Meta Data

--- warriorbeat/setup.py
+++ warriorbeat/setup.py
@@ -3,6 +3,8 @@ from setuptools import setup
 setup(
     name='warriorbeat',
     packages=['warriorbeat'],
+    author='Braden Mars',
+    url='https://github.com/WarriorBeat/WarriorBeatApi',
     include_package_data=True,
     install_requires=[
         'flask',
