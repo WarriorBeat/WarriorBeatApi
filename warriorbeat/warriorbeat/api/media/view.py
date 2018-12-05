51c538fa4c93749cb81010a385856cd598f39de3
Braden Mars <bradenmars02@gmail.com>
2018-12-05 00:14:57 -0600

    Media Resource CRUD & Bug Fixes
    - Media Resource - 
    * [Added] patch method for handling updates
    * [Added] delete method for handling deletes
    * [Fixed] key attribute adding on to itself on update
    
    - Tests - 
    * [Added] Media edit test
    * [Added] Media delete test

--- warriorbeat/warriorbeat/api/media/view.py
+++ warriorbeat/warriorbeat/api/media/view.py
@@ -8,7 +8,7 @@ from flask_restful import Resource
 
 from warriorbeat.api.media.model import Media
 from warriorbeat.api.media.schema import ImageSchema
-from warriorbeat.utils import use_schema
+from warriorbeat.utils import parse_json, retrieve_item, use_schema
 
 
 class MediaList(Resource):
@@ -23,6 +23,19 @@ class MediaList(Resource):
 
 
 class MediaItem(Resource):
-    def get(self, mediaId):
-        media = Media.retrieve(mediaId)
+
+    @retrieve_item(Media)
+    def get(self, media, **kwargs):
         return media
+
+    @parse_json
+    @retrieve_item(Media, ImageSchema)
+    def patch(self, data, media, **kwargs):
+        media = media.update(data)
+        media.set_source()
+        return media.save()
+
+    @retrieve_item(Media, ImageSchema)
+    def delete(self, media, **kwargs):
+        media.delete()
+        return '', 204
