0924c4dd3d22ecea5809741cfac53691a6413814
Braden Mars <BradenM@users.noreply.github.com>
2018-12-20 22:19:08 -0600

    Version Bump @ v0.6.4

--- warriorbeat/setup.py
+++ warriorbeat/setup.py
@@ -4,7 +4,7 @@ setup(
     name='warriorbeat',
     packages=['warriorbeat'],
     author='Braden Mars',
-    version="0.6.3",
+    version="0.6.4",
     url='https://github.com/WarriorBeat/WarriorBeatApi',
     include_package_data=True,
     install_requires=[
